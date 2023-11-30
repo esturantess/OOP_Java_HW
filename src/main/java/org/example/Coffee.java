b11b72b7b715eefad27e4de69226351df4b3359a
Polina <ms.maylin@inbox.ru>
2023-11-30 22:00:07 +0300

    Classes Coffee and HotDrink added

---
+++ src/main/java/org/example/Coffee.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class Coffee extends HotDrink{
+    private int temperature;
+}
---
+++ src/main/java/org/example/HotDrink.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public abstract class HotDrink {
+    private String name;
+    private int cost;
+}
