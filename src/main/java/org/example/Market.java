5125f9b3fcc7771b4bc2fa5c8f616cca567f23cb
Polina <ms.maylin@inbox.ru>
2023-12-08 01:11:52 +0300

    class market added

---
+++ src/main/java/org/example/Market.java
@@ -0,0 +1,67 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Market implements QueueBehaviour, MarketBehaviour{
+
+    List<Actor> queue = new ArrayList<>();
+    @Override
+    public void acceptToMarket(Actor actor) {
+        System.out.println(actor.getName() + " пришел в магазин.");
+        takeInQueue(actor);
+    }
+
+    @Override
+    public void releaseFromMarket(List<Actor> actors) {
+        for(Actor actor: actors){
+            queue.remove(actor);
+            System.out.println(actor.getName() + " вышел из магазина.");
+        }
+    }
+
+    @Override
+    public void update() {
+        takeOrders();
+        giveOrders();
+        releaseFromQueue();
+    }
+
+    @Override
+    public void takeInQueue(Actor actor) {
+        queue.add(actor);
+        System.out.println(actor.getName() + " встал в очередь.");
+    }
+
+    @Override
+    public void takeOrders() {
+        for(Actor actor: queue){
+            if(!actor.isMakeOrder){
+                actor.setMakeOrder(true);
+                System.out.println(actor.getName() + " сделал заказ.");
+            }
+        }
+    }
+
+    @Override
+    public void giveOrders() {
+        for(Actor actor: queue){
+            if(!actor.isMakeOrder){
+                actor.setTakeOrder(true);
+                System.out.println(actor.getName() + " получил заказ.");
+            }
+        }
+    }
+
+    @Override
+    public void releaseFromQueue() {
+        List<Actor> releasedActors = new ArrayList<>();
+        for (Actor actor: queue){
+            if (actor.isTakeOrder){
+                releasedActors.add(actor);
+                System.out.println(actor.getName() + " вышел из очереди.");
+            }
+        }
+        releaseFromMarket(releasedActors);
+    }
+}
