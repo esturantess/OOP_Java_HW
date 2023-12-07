68a0dd4d9b3a1259e0b784bc28150510083c1e8d
Polina <ms.maylin@inbox.ru>
2023-12-08 00:04:57 +0300

    class actor added

---
+++ src/main/java/org/example/Actor.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public abstract class Actor {
+    protected String name;
+    protected boolean isMakeOrder;
+    protected boolean isTakeOrder;
+    abstract String getName();
+}
