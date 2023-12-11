a4bfe4e722199629cef63a4f9193f386c57db68b
Polina <ms.maylin@inbox.ru>
2023-12-12 01:00:28 +0300

    class StreamComparator updated

--- src/main/java/org/example/StreamComparator.java
+++ src/main/java/org/example/StreamComparator.java
@@ -2,9 +2,11 @@ package org.example;
 
 import java.util.Comparator;
 
-public class StreamComparator implements Comparator<Stream> {
+public class StreamComparator implements Comparator<Stream>{
+
     @Override
-    public int compare(Stream o1, Stream o2) {
-        return 0;
+    public int compare(Stream arg0, Stream arg1) {
+        return Integer.compare(arg0.getListStudentGroup().size(), arg1.getListStudentGroup().size());
     }
+
 }
