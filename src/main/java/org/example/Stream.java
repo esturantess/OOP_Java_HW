81feb41e3b3f7a78755d61c2cf96f41a6927a73f
Polina <ms.maylin@inbox.ru>
2023-12-12 00:59:58 +0300

    class Stream updated

--- src/main/java/org/example/Stream.java
+++ src/main/java/org/example/Stream.java
@@ -1,11 +1,50 @@
 package org.example;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Spliterator;
-import java.util.function.Consumer;
 
-public class Stream implements Iterable<StudentGroup>{
-    private List<StudentGroup> StudyGroups;
+public class Stream implements Iterator<StudentGroup> {
+    private List<StudentGroup> listStudentGroup;
+
+    public List<StudentGroup> getListStudentGroup() {
+        return listStudentGroup;
+    }
+
+    public void setListStudentGroup(List<StudentGroup> listStudentGroup) {
+        this.listStudentGroup = listStudentGroup;
+    }
+
+    private int index;
+
+    public Stream(List<StudentGroup> studentsStreame) {
+        this.listStudentGroup = studentsStreame;
+        this.index = -1;
+    }
+
+    public Stream() {
+        this.listStudentGroup = new ArrayList<>();
+        this.index = -1;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < listStudentGroup.size() - 1;
+    }
+
+    @Override
+    public StudentGroup next() {
+        if (!hasNext()) {
+            return null;
+        }
+        return listStudentGroup.get(++index);
+    }
+
+    @Override
+    public void remove() {
+        if (index >= 0 && index < listStudentGroup.size()) {
+            listStudentGroup.remove(index);
+        }
+    }
 
 }
