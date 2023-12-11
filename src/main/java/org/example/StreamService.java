7cbca3b972298468d8e3fd24d693b5c8e3dcc91b
Polina <ms.maylin@inbox.ru>
2023-12-12 01:01:02 +0300

    class StreamService updated

--- src/main/java/org/example/StreamService.java
+++ src/main/java/org/example/StreamService.java
@@ -1,4 +1,16 @@
 package org.example;
 
+import java.util.List;
+
 public class StreamService {
+    private List<Stream> listStreams;
+
+    public StreamService(List<Stream> listStreams) {
+        this.listStreams = listStreams;
+    }
+
+    public void sortStudentsStreamBySize(){
+        listStreams.sort(new StreamComparator());
+    }
+
 }
