00704c92ed5442a51049a30e70a81456d0da93d8
Polina <ms.maylin@inbox.ru>
2023-12-12 00:37:34 +0300

    Class stream added

---
+++ src/main/java/org/example/Stream.java
@@ -0,0 +1,11 @@
+package org.example;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class Stream implements Iterable<Integer>{
+    private List<Integer> StudyGroups;
+
+}
